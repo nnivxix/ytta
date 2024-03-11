1c5ff86aecc78a421a59acf06560a76512b249da
nnivxix <adhe.hanasa@gmail.com>
2024-03-11 10:47:31 +0700

    feat: create table `videos, posts, comments` table

---
+++ database/migrations/2024_03_11_033525_create_videos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->foreignId('user_id');
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('path')->nullable();
+            $table->boolean('is_public')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
