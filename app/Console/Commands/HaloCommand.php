197c2ce537d618f60817a2b38e13ec75c84a58ce
nnivxix <adhe.hanasa@gmail.com>
2024-03-26 09:58:10 +0700

    wip: halo command

---
+++ app/Console/Commands/HaloCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use function Laravel\Prompts\text;
+
+class HaloCommand extends Command
+{
+    protected $signature = 'app:halo';
+
+    protected $description = 'Command description';
+
+    public function handle()
+    {
+        $name = text(
+            label: 'What is your name?',
+            placeholder: 'E.g. Taylor Otwell',
+            hint: 'This will be displayed on your profile.'
+        );
+        $this->info($name);
+    }
+}
